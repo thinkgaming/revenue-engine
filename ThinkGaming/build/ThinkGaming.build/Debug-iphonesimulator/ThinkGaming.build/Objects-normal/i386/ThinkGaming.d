5f8e0f9d7b800b007f278c7050115da3cb700392
Daniel Wyszynski <dan@punch.is>
2013-05-21 15:40:35 -0400

    Removed intermediate files that shouldn't get into the repo. [DW]

--- ThinkGaming/build/ThinkGaming.build/Debug-iphonesimulator/ThinkGaming.build/Objects-normal/i386/ThinkGaming.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-  /Volumes/Macintosh\ HD/Users/dan/Code/ThinkGaming/ThinkGaming/ThinkGaming/ThinkGaming.m \
-  /Volumes/Macintosh\ HD/Users/dan/Code/ThinkGaming/ThinkGaming/ThinkGaming/ThinkGaming.h \
-  /Volumes/Macintosh\ HD/Users/dan/Code/ThinkGaming/ThinkGaming/../AFNetworking/AFHTTPClient.h \
-  /Volumes/Macintosh\ HD/Users/dan/Code/ThinkGaming/ThinkGaming/../AFNetworking/AFURLConnectionOperation.h \
-  /Volumes/Macintosh\ HD/Users/dan/Code/ThinkGaming/ThinkGaming/../AFNetworking/AFJSONRequestOperation.h \
-  /Volumes/Macintosh\ HD/Users/dan/Code/ThinkGaming/ThinkGaming/../AFNetworking/AFHTTPRequestOperation.h \
-  /Volumes/Macintosh\ HD/Users/dan/Code/ThinkGaming/ThinkGaming/../Reachabilty/Reachability.h
