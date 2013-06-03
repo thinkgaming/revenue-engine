5e58f6c16e4eea356bd9468251cc2e17e1eda96b
Aaron Junod <ajunod@sharedeets.com>
2013-06-03 09:55:36 -0400

    added test harness project

---
+++ ThinkGamingTestHarness/ThinkGamingTestHarness/ThinkGamingTestHarness/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ThinkGamingTestHarness
+//
+//  Created by Aaron Junod on 6/3/13.
+//  Copyright (c) 2013 thinkgaming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ThinkGamingTestHarness/ThinkGamingTestHarness/ThinkGamingTestHarness/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ThinkGamingTestHarness
+//
+//  Created by Aaron Junod on 6/3/13.
+//  Copyright (c) 2013 thinkgaming. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
