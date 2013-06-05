1cb8fbec3e2affdc006f92837dba78561255141e
Aaron Junod <ajunod@sharedeets.com>
2013-06-05 09:55:20 -0400

    copied the newewst version to the lib folder

--- Lib/ThinkGaming.h
+++ Lib/ThinkGaming.h
@@ -8,29 +8,24 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-#import "AFHTTPClient.h"
 
-@interface ThinkGaming : AFHTTPClient {
-    NSMutableArray *queue;
-    NSTimer *dispatchTimer;
-    NSString *apiKey;
-}
+@interface ThinkGaming : NSObject
 
 + (ThinkGaming *)sharedSingleton;
 
 /*
- start session, attempt to send saved sessions to server
+ start session with given API key (get your API key from your dashboard)
  */
 + (void)startSession:(NSString *)apiKey;
 
 /*
- log events after session has started
+ log events after session has started. If passing parameters, simply make a dictionary with the key-value pairs as you wish for them to appear in your event dashboard.
  */
 + (void)logEvent:(NSString *)eventName;
 + (void)logEvent:(NSString *)eventName withParameters:(NSDictionary *)parameters;
 
 /*
- start or end timed events
+ start or end timed events. NOTE: For event start and end times to be paired properly, the eventName must match in the logEvent and endTimedEvent calls.
  */
 + (void)logEvent:(NSString *)eventName timed:(BOOL)timed;
 + (void)logEvent:(NSString *)eventName withParameters:(NSDictionary *)parameters timed:(BOOL)timed;
